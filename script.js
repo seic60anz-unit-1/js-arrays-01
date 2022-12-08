301269c51d137ac62d41b3f62a667ff173ada6ea
Wing Fung Lau <wingfunglau@Wings-MacBook-Pro.local>
2022-12-08 14:45:34 +1100

    Movie quotes are too long

---
+++ script.js
@@ -0,0 +1,9 @@
+//Movie Quotes
+const quotes = ["Blind Chance",
+"Vertigo", "Santa Sangre", "Trafic"]
+console.log(quotes[1])
+console.log(quotes[quotes.length - 1])
+console.log(quotes.length)
+quotes[0] = "Good Morning"
+console.log(quotes) //Movie quotes are too long, so I put movie names as quotes instead
+
