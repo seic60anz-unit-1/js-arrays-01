30e9029f38ac24c3e8380bda38e06612dfe31768
Wing Fung Lau <wingfunglau@Wings-MacBook-Pro.local>
2022-12-08 14:50:39 +1100

    for vs for..of

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 //Movie Quotes
-const quotes = ["Blind Chance",
-"Vertigo", "Santa Sangre", "Trafic"]
-console.log(quotes[1])
-console.log(quotes[quotes.length - 1])
-console.log(quotes.length)
-quotes[0] = "Good Morning"
-console.log(quotes) //Movie quotes are too long, so I put movie names as quotes instead
+// const quotes = ["Blind Chance",
+// "Vertigo", "Santa Sangre", "Trafic"]
+// console.log(quotes[1])
+// console.log(quotes[quotes.length - 1])
+// console.log(quotes.length)
+// quotes[0] = "Good Morning"
+// console.log(quotes) //Movie quotes are too long, so I put movie names as quotes instead
 
+//for vs for..of
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+for (let i = 0; i < letters.length; i++){
+    console.log(`${i} - ${letters[i]}`)
+}
+for (let i of letters){
+    console.log(i)
+}
